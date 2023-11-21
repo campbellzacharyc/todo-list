8f3c37f6be6a96d28e6fa3057f1e478d991cdcac
zacharyccampbell <zacharydenzel23@gmail.com>
2023-11-21 15:48:42 -0600

    Done?

--- src/createTodo.js
+++
@@ -1,7 +0,0 @@
-
-export const createTodoItem = (title, description, dueDate, priority, checkList) => {
-    let todoList = [];
-    todoList.push({ title, description, dueDate, priority, checkList});
-    return { title, description, dueDate, priority, checkList }; 
-}
-
--- src/tabs.js
+++
@@ -1,8 +0,0 @@
-
-export function clearContent() {
-    const content = document.querySelector('#content');
-    const projectContent = document.querySelector('project');
-    if (projectContent) {
-        content.removeChild(projectContent);
-    }
-}
